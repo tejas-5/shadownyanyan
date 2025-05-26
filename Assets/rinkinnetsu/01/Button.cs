f9b04e08384b0fcd04e5e5bdfc1a0b01e83f59f3
kiki1344 <game22403019@gmail.com>
2025-05-26 12:16:10 +0900

    ui

---
+++ Assets/rinkinnetsu/01/Button.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Button: MonoBehaviour
+{
+    public void StartgameButton()
+    {
+        Debug.Log("GameStart");
+        SceneManager.LoadScene("Test");
+    }
+
+    public void ExicGameButton()
+    {
+        Debug.Log("GameExic");
+        Application.Quit();
+    }
+}
