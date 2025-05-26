29a7ca8d53ef35468acc21ec74890ba0f02cb943
tsuruyosi <tsukitaka496@gmail.com>
2025-05-26 12:16:15 +0900

    tsuruyosi
    Player関係とリスポーン地点について

---
+++ Assets/tsuruyosi/Spirct/Player/PlayerJump.cs
@@ -0,0 +1,96 @@
+using UnityEngine;
+
+public class PlayerJump : MonoBehaviour
+{
+    [SerializeField] ShadowSwitch shadowSwitch;
+    [SerializeField] GameObject groundCheckPos;
+    [SerializeField] float fallMultiplier; //落ちるときの速度の乗数
+    [SerializeField] float jumpMultiplier; //ジャンプして上がるときの速度の乗数
+    [SerializeField] float jumpForce; //ジャンプの力
+    [SerializeField] float jumpTime; //ジャンプしていられる時間
+    [SerializeField] float checkRadius; //地面接地の取得範囲
+    public LayerMask Ground;
+
+    Vector2 vecGavity;
+    Rigidbody2D rb;
+
+    float jumpCounter;
+
+    bool isJumping;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        vecGavity = new Vector2(0, -Physics2D.gravity.y);
+    }
+
+    void Update()
+    {
+        if (shadowSwitch.b_move == false)
+        {
+            JumpHandler();
+        }
+
+        //キャラクターの落ちる速度を徐々に増加させる
+        if (rb.linearVelocity.y < 0)
+        {
+            rb.linearVelocity -= vecGavity * fallMultiplier * Time.deltaTime;
+        }
+
+        //ボタンを押した時間によってジャンプの高さが変わるようにする
+        if (rb.linearVelocity.y > 0 && isJumping)
+        {
+            jumpCounter += Time.deltaTime;
+
+            if (jumpCounter > jumpTime)
+            {
+                isJumping = false;
+            }
+
+            float t = jumpCounter / jumpTime;
+            float currentJumpM = jumpMultiplier;
+
+            //ジャンプの半分に達したら徐々に上がる速度を落とす
+            if (t > 0.5)
+            {
+                currentJumpM = jumpMultiplier * (1 - t);
+            }
+
+            rb.linearVelocity += vecGavity * currentJumpM * Time.deltaTime;
+        }
+
+    }
+
+    //ジャンプ関数
+    private void JumpHandler()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (isGrounded())
+            {
+
+                isJumping = true;
+
+                jumpCounter = 0;
+
+                Jump();
+            }
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            isJumping = false;
+        }
+    }
+
+    private void Jump()
+    {
+        rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+    }
+
+    //地面との接地をチェック1
+    private bool isGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheckPos.transform.position, checkRadius, Ground);
+    }
+}
---
+++ Assets/tsuruyosi/Spirct/Player/PlayerMovement.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] ShadowSwitch shadowSwitch;
+    [SerializeField] float speed;
+    int directionScale = 1; //Player‚ÌŒü‚«
+
+    private void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if(shadowSwitch.b_move == false)
+        {
+            Move();
+        }
+    }
+
+    private void Move()
+    {
+        Vector3 inputVector = new Vector3(0, 0, 0);
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputVector.x = 1;
+            transform.localScale = new Vector2(directionScale, 1); //Œü‚«
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputVector.x = -1;
+            transform.localScale = new Vector2(-directionScale, 1); //Œü‚«
+        }
+
+        inputVector = inputVector.normalized;
+        transform.position += inputVector * speed * Time.deltaTime;
+
+    }
+}
