3f05efd260bde04a862a849651e249028078d8a2
Patcharida07 <game22403030@gmail.com>
2025-06-23 02:18:37 +0900

    nana
    lavel1 fin

---
+++ Assets/nana/Scripts/RealScripts/KeyPickup.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class KeyPickup : MonoBehaviour
+{
+    public enum OwnerType { Player, Shadow }
+    public OwnerType owner;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (owner == OwnerType.Player && other.CompareTag("Player"))
+        {
+            Debug.Log("Player เก็บกุญแจ");
+            KeyManager.instance.PickupKey(KeyManager.OwnerType.Player);  // เรียก enum จาก KeyManager
+            Destroy(gameObject);
+        }
+        else if (owner == OwnerType.Shadow && other.CompareTag("Shadow"))
+        {
+            Debug.Log("Shadow เก็บกุญแจ");
+            KeyManager.instance.PickupKey(KeyManager.OwnerType.Shadow);  // เรียก enum จาก KeyManager
+            Destroy(gameObject);
+        }
+    }
+}
+
+
---
+++ Assets/nana/Scripts/RealScripts/LiftController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class LiftController : MonoBehaviour
+{
+    public Transform topPoint;
+    public Transform bottomPoint;
+    public float speed = 2f;
+
+    private bool goingUp = false;
+    private bool moving = false;
+
+
+    void Update()
+    {
+        if (!moving) return;
+
+        Vector3 target = goingUp ? topPoint.position : bottomPoint.position;
+        transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, target) < 0.01f)
+        {
+            moving = false; // หยุดเมื่อถึงจุดหมาย
+        }
+
+        Debug.Log($"GoingUp: {goingUp}, Moving: {moving}");
+        Debug.Log($"Lift Pos: {transform.position}, Target: {(goingUp ? topPoint.position : bottomPoint.position)}");
+
+    }
+
+    public void Toggle()
+    {
+        goingUp = !goingUp;
+        moving = true;
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        if (topPoint != null && bottomPoint != null)
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawSphere(topPoint.position, 0.2f);
+
+            Gizmos.color = Color.red;
+            Gizmos.DrawSphere(bottomPoint.position, 0.2f);
+        }
+    }
+}
