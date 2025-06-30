9df5e905cf39a5209cce90f3bb271c575633f324
Patcharida07 <game22403030@gmail.com>
2025-06-30 12:13:55 +0900

    nana

---
+++ Assets/nana/ScriptsLevel2/PlatformWaveTrigger.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlatformWaveTrigger : MonoBehaviour
+{
+    public float delayBetweenEach = 0.3f;
+    public Transform moveTargetPoint;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Player entered ArrivalZone! เริ่มเคลื่อนพื้น");
+            StartCoroutine(MovePlatformsOneByOne());
+        }
+    }
+
+    IEnumerator MovePlatformsOneByOne()
+    {
+        var platforms = Object.FindObjectsByType<MovingPlatformControlled>(FindObjectsSortMode.None);
+
+        List<MovingPlatformControlled> sorted = new List<MovingPlatformControlled>(platforms);
+        sorted.Sort((a, b) => a.transform.position.x.CompareTo(b.transform.position.x));
+
+        foreach (var platform in sorted)
+        {
+            if (platform != null)
+            {
+                platform.StartMoving(moveTargetPoint.position);
+                yield return new WaitForSeconds(delayBetweenEach);
+            }
+        }
+    }
+}
---
+++ Assets/nana/ScriptsLevel2/PlayerPlatformAttach.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class PlayerPlatformAttach : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // ถ้าแตะพื้นที่ชื่อมีคำว่า "Platform"
+        if (collision.gameObject.CompareTag("Platform"))
+        {
+            transform.SetParent(collision.transform);  // ทำให้เป็นลูกของพื้น
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        // เมื่อออกจากพื้น
+        if (collision.gameObject.CompareTag("Platform"))
+        {
+            transform.SetParent(null);  // ปลด parent ออก
+        }
+    }
+}
