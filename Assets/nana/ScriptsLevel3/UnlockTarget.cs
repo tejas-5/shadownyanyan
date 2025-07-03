b160b28d73f216ae0d3afe630775a710826d4863
patcharida <game22403030@gmail.com>
2025-07-03 16:39:46 +0900

    nana

---
+++ Assets/nana/ScriptsLevel3/UnlockTarget.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class UnlockTarget : MonoBehaviour
+{
+    public GameObject hiddenPuzzlePlate; // แผ่นที่ 4 ที่จะปรากฏ
+
+    public void Unlock()
+    {
+        Debug.Log("เปิดกล่อง/ประตูเรียบร้อย!");
+
+        // วิธีที่ 1: ทำให้กล่องหายไป
+        gameObject.SetActive(false);
+
+        // วิธีที่ 2: หรือใส่อนิเมชั่นเปิด (แล้วใช้ Animation Event ก็ได้)
+
+        // แสดงแผ่นพัซเซิล
+        if (hiddenPuzzlePlate != null)
+        {
+            hiddenPuzzlePlate.SetActive(true);
+        }
+    }
+}
