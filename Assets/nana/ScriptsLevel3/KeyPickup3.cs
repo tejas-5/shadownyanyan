4b4e0facd50771f8cb16225efa97738fe7ca82c5
patcharida <game22403030@gmail.com>
2025-07-03 16:39:46 +0900

    nana

---
+++ Assets/nana/ScriptsLevel3/KeyPickup3.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class KeyPickup3 : MonoBehaviour
+{
+    public GameObject targetToUnlock; // กล่องหรือประตู
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("เก็บกุญแจแล้ว!");
+            Destroy(gameObject); // ลบกุญแจ
+
+            if (targetToUnlock != null)
+            {
+                Destroy(targetToUnlock); // ลบกล่องออกจากฉาก
+            }
+        }
+    }
+}
