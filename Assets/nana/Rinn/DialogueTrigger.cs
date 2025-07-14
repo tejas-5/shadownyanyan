ba9c99e34576ccef488e59451979ba3ab3a50203
kiki344 <game22403019@gmail.com>
2025-07-14 11:55:07 +0900

    ui
    77777

---
+++ Assets/nana/Rinn/DialogueTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public List<DialogueLine> dialogueLines;
+
+    private bool triggered = false;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (triggered)
+            return;
+
+        if (other.CompareTag("Player"))
+        {
+            triggered = true;
+
+            DialogueManager manager = FindObjectOfType<DialogueManager>();
+            if (manager != null)
+            {
+                manager.StartDialogue(dialogueLines);
+            }
+            else
+            {
+                Debug.LogError("场景中没有DialogueManager");
+            }
+        }
+    }
+}
