6090127945353bdf6d1c433a882761d91d873abe
tejas-5 <game22403048@gmail.com>
2025-08-25 11:31:27 +0900

    bug fixes

--- Assets/nana/Scripts/RealScripts/LightSwitch.cs
+++ Assets/nana/Scripts/RealScripts/LightSwitch.cs
@@ -1,5 +1,4 @@
 ﻿using UnityEngine;
-using UnityEngine.Rendering.Universal;
 
 public class LightSwitch : MonoBehaviour
 {
@@ -8,20 +7,13 @@ public class LightSwitch : MonoBehaviour
     private bool isPlayerInRange = false;
     private bool isLightOn = false;
 
-    private Light2D light2D;
-
     void Start()
     {
         if (lightToTurnOn != null)
         {
-            light2D = lightToTurnOn.GetComponent<Light2D>();
-
-            if (light2D != null)
-            {
-                // Start with light off
-                light2D.intensity = 0f;
-                light2D.enabled = false;
-            }
+            // Start with the light turned off
+            lightToTurnOn.SetActive(false);
+            isLightOn = false;
         }
     }
 
@@ -29,27 +21,12 @@ public class LightSwitch : MonoBehaviour
     {
         if (isPlayerInRange && Input.GetKeyDown(KeyCode.E))
         {
-            if (light2D != null)
-            {
-                isLightOn = !isLightOn;
+            isLightOn = !isLightOn;
 
-                if (isLightOn)
-                {
-                    light2D.enabled = true;
-                    light2D.intensity = 1f;
-                }
-                else
-                {
-                    light2D.intensity = 0f;
-                    light2D.enabled = false;
-                }
-
-                Debug.Log(isLightOn ? "ไฟเปิดแล้ว!" : "ไฟปิดแล้ว!");
-            }
-            else if (lightToTurnOn != null)
+            if (lightToTurnOn != null)
             {
-                isLightOn = !isLightOn;
                 lightToTurnOn.SetActive(isLightOn);
+                Debug.Log(isLightOn ? "ไฟเปิดแล้ว!" : "ไฟปิดแล้ว!");
             }
         }
     }
