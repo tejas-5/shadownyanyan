3715e20bdca5abf4d8318cd85b02cc9fe7c94102
Patcharida07 <game22403030@gmail.com>
2025-08-30 03:04:41 +0900

    nnn

---
+++ Assets/nana/Scripts/RealScripts/GlowBlink.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class GlowBlink : MonoBehaviour
+{
+    public Color baseColor = Color.yellow;   // สีปกติ (เหลืองสำหรับกุญแจ)
+    public Color glowColor = Color.white;   // สีเรืองแสง
+    public float speed = 2f;                // ความเร็วในการวิ๊บ
+
+    private SpriteRenderer sr;
+
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        // เปลี่ยนสีไปมาระหว่าง baseColor และ glowColor
+        sr.color = Color.Lerp(baseColor, glowColor, Mathf.PingPong(Time.time * speed, 1));
+    }
+}
